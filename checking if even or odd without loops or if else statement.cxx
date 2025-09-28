75ddda226859e00cedc6d0757497e6c74ee0f1ab
garvsharma1504-sudo <garvsharma1504@gmail.com>
2025-09-28 20:01:25 +0530

    Update checking if even or odd without loops or if else statement.cxx

--- checking if even or odd without loops or if else statement.cxx
+++ checking if even or odd without loops or if else statement.cxx
@@ -5,11 +5,13 @@ int main(){
     //defining variables to be used:
     int x;
     std::string integer[]={"even","odd"};
+    
     //getting user input:
     std::cout << " Enter a number to check even or odd::";
     std::cin >> x;
-    //if no is odd :: index(1) is shown which us odd :-
-    //if no is even :: index(0) is shown which us even :-
+    
+    //if no is odd :: index(1) is shown which is odd :-
+    //if no is even :: index(0) is shown which is even :-
     std::cout << integer[x%2];
     
     return 0;
